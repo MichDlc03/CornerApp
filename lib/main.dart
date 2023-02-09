2a4f30d36436c1ac0609095f42524b1bdf2de0eb
Martin Lugo <martinyahir240918@gmail.com>
2023-02-08 18:06:47 -0600

    Martin 1.1
    version 1.1 Add SettingProfile, edit_profile, configresponsive, lenguaje.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:liga_corner_app/pages/edit_profile.dart';
+import 'package:liga_corner_app/pages/settingsProfile.dart';
 import 'package:liga_corner_app/widgets/nav_bar.dart';
 
 void main() {
@@ -11,9 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const  MaterialApp(
+    return   MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/settinsProfile': (context) => const SettingsProfilePage(),  
+        '/edid_Profile': (context) => const EdidProfile()
+      },
       debugShowCheckedModeBanner: false,
-      home: NavigationExample(), 
+      home:const  NavigationExample(), 
+
     );
   }
 }
